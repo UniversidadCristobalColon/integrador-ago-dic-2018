fc62c424df0a18f1a53a1e7bed606e3e3a810c4e
JudyFararoni <j-fara@hotmail.com>
2018-11-07 08:26:57 -0600

    egresos

--- admin/pos/egresos/editar.php
+++ admin/pos/egresos/editar.php
@@ -41,7 +41,7 @@
                 </div>
             </div>
             <div class="row mb-3">
-                <div class="col-md-6">
+                <div class="col-md-4">
                     <label for="inputUsuario">Usuario: </label>
                     <select class="custom-select d-block w-100" id="usuario" required>
                         <option value="">Selecciona...</option>
@@ -51,7 +51,13 @@
                         <option>Recepción</option>
                     </select>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-3">
+                    <label for="inputMonto">Fecha: </label>
+                    <div>
+                        <input class="form-control" type="date" value="2011-08-19" id="example-date-input">
+                    </div>
+                </div>
+                <div class="col-md-3">
                     <label for="inputMonto">Monto: </label>
                     <input type="text" class="form-control" id="inputMonto" placeholder="Monto" required>
                 </div>
