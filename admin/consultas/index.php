9ce2c53425002cc6f3c49a76b0b8e241a80cc6a6
FernandoMojica <fernando_mojica_94@hotmail.com>
2018-11-08 21:21:04 -0600

    Se agrego Datatable a ambos archivos PHP.

--- admin/consultas/index.php
+++ admin/consultas/index.php
@@ -19,6 +19,23 @@
     <!-- Archivos JS -->
     <script src="../../js/base.js"></script>
 
+    <!--Data table -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
+    <script
+            src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js">
+    </script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js">
+    </script>
+    <script>
+        $(document).ready( function () {
+            $('#example').DataTable();
+        } );
+    </script>
+
 </head>
 
 <body>
@@ -30,17 +47,17 @@
     <div class="starter-template">
         <h1>Historial de pagos</h1>
 
-        <div class="pull-right">
+        </br><div class="pull-right">
             <a href="index.php" class="btn btn-default-btn-xs btn-primary"><i class="glyphicon glyphicon-refresh"></i> Actualizar</a>
             <a href= "situacion_clientes.php" class="btn btn-default-btn-xs btn-success"><i class="glyphicon glyphicon-back"></i> Estado de los clientes</a>
-        </div>
+        </div></br>
 
     </div>
 
     <div class="container">
         <div class="row">
             <div class="table-responsive">
-                <table class="table table-bordered">
+                <table id="example" class="table table-bordered">
                     <thead>
                     <tr>
                         <th>#</th>
@@ -98,7 +115,6 @@
 
 </main>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
