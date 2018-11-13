47539f8bafc920e772c9eb9e43358e830834e5e3
JudyFararoni <j-fara@hotmail.com>
2018-11-13 09:41:44 -0600

    php's

---
+++ admin/pos/egresos/eliminar.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Judith
+ * Date: 13/11/2018
+ * Time: 09:40 AM
+ */
+require ("conexion.php");
+$id = $_POST["idEliminar"];
+
+$query = "delete from egresos where id=$id";
+
+$result = pg_query($dbcon,$query);
+
+if ($result){
+    header("Location: index.php");
+}else{
+    echo "No se pudo llevar a acabo la operacion.";
+    header("refresh:2; url=index.php");
+}
