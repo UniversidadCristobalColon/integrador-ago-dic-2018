46d4f01dfe193bdca35bc96d1c3f9a45af12a338
José Rubén Sánchez González <jrsg@outlook.com>
2018-11-26 18:38:29 -0600

    Se corrige la conexión a la base de datos remota

--- scripts/config.php
+++ scripts/config.php
@@ -1,13 +1,12 @@
 <?php
 session_start();
 
-error_reporting(E_ALL);
-ini_set('display_errors', 'on');
+/*error_reporting(E_ALL);
+ini_set('display_errors', 'on');*/
 
 define('BASE', '//' . $_SERVER['HTTP_HOST'] . '/');
 
-//$db = mysqli_connect("35.225.5.28", "sarx_user", "oBSH6d4RicpMR8Ja", "sarx_db");
-$db = mysqli_connect('localhost', 'root','root','sarx_db');
+$db = mysqli_connect("35.225.5.28", "sarx_user", "oBSH6d4RicpMR8Ja", "sarx_db");
 
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
