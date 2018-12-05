819e59fed89f04a8647b44defc04782b5751bbd1
Janeth Santiago <janeth_lsanti@hotmail.com>
2018-12-04 18:35:29 -0600

    Ingresos

--- admin/pos/ingresos/agrega_ingreso.php
+++ admin/pos/ingresos/agrega_ingreso.php
@@ -7,11 +7,14 @@
     <meta name="description" content="Sistema de gestión de Sarx Wellness Center">
     <meta name="author" content="UCC Sistemas">
 
-    <title>Sarx Wellness Center</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="icon" href="../../../img/favicon.png">
 
-    <link rel="icon" href="../img/favicon.png">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/gijgo@1.9.10/js/gijgo.min.js" type="text/javascript"></script>
+    <link href="https://cdn.jsdelivr.net/npm/gijgo@1.9.10/css/gijgo.min.css" rel="stylesheet" type="text/css" />
 
     <!-- Hojas de estilos -->
     <link href="../../../css/base.css" rel="stylesheet">
@@ -27,42 +30,70 @@
 
 <main role="main" class="container">
 
-    <h1>Nuevo Ingreso</h1>
+    <div class="starter-template">
+        <h1>Nuevo ingreso</h1>
+        <p class="lead">
+        <form action="nuevo_ing.php" method="post">
+            <button type="submit" class="btn btn-success">Guardar</button>
+            <div class="row mb-3">
+                <div class="col-md-10">
+                    <br>
+                    <label for="inputDescripcion">Descripción: </label>
+                    <input type="text" name="descripcion" class="form-control" id="inputDescripcion" placeholder="Descripción" required>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-md-4">
+                    <label for="inputUsuario">Usuario: </label>
+                    <select class="custom-select d-block w-100" name="user" id="usuario" required>
+                        <option value="">Selecciona...</option>
+                        <?php
+                        $query="select * from usuarios";
+                        $result=mysqli_query($db,$query);
+                        while ($valores=mysqli_fetch_assoc($result)) {
+                            echo '<option value="'.$valores[id_usuario].'">'.$valores[nombre_corto].'</option>';
+                        }
+                        ?>
+                    </select>
+                </div>
+                <div class="col-md-3">
+                    <label for="inputMonto">Fecha: </label>
+                    <input id="datepicker" name="fecha">
+                    <script>
+                        $('#datepicker').datepicker({
+                            uiLibrary: 'bootstrap4',
+                            format: 'yyyy-mm-dd'
+                        });
+                        $("#datepicker").attr("readonly", true);
+                    </script>
+                </div>
+                <div class="col-md-3">
+                    <label for="inputMonto">Monto: </label>
+                    <input type="text" name="importe" class="form-control" id="inputMonto" placeholder="100.00" required>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-md-3">
+                    <label for="inputCliente">ID Cliente: </label>
+                    <input type="text" name="cliente" class="form-control" id="inputMonto" placeholder="1" required>
+                </div>
+                <div class="col-md-3">
+                    <label for="inputMonto">Cantidad: </label>
+                    <input type="text" name="cantidad" class="form-control" id="inputMonto" placeholder="1" required>
+                </div>
+            </div>
+        </form>
+        </p>
+    </div>
 
-<h4>Actividad de ingreso</h4>
-    <div class="form-group">
-        <label for="exampleFormControlSelect1">Producto</label>
-        <select class="form-control" id="exampleFormControlSelect1">
-            <option></option>
-            <option>Mensualidad</option>
-            <option>Membresia</option>
-            <option>Agua</option>
-        </select>
-    </div>
-    <div class="form-group">
-        <label for="exampleFormControlSelect1">Cantidad</label>
-        <select class="form-control" id="exampleFormControlSelect1">
-            <option></option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-        </select>
-    </div>
-    <div class="form-group">
-        <label for="producto">Cliente</label>
-        <input type="text" class="form-control" id="producto" aria-describedby="" placeholder="">
-    </div>
-    <div class="form-group">
-        <label for="producto">Precio $</label>
-        <input type="text" class="form-control" id="producto" aria-describedby="" placeholder="">
-    </div>
-
-    <input class="btn btn-primary" type="submit" value="Guardar">
 </main>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
 </body>
 </html>
