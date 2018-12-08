a5ef2c9acfcf6942d7ab555d1c8919d405c38659
DalsyJLQR <dals_1060@hotmail.com>
2018-12-08 11:42:35 -0600

    Ruta del archivo eventoClases.php ajustado Fixes #79

--- admin/clases/index.php
+++ admin/clases/index.php
@@ -48,7 +48,7 @@
                 editable: true,
                 eventLimit: true, // allow "more" link when too many events
                 events:
-                    'http://localhost:8888/int2018/admin/clases/eventoClases.php'
+                    'eventoClases.php'
             });
         });
 
